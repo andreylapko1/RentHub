60fdab81a9df854d08c68feedb8e507dccde4302
AndreyLapko1 <andreycgo765@gmail.com>
2025-04-01 16:24:50 +0300

    remake

--- listings/models.py
+++ listings/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Listing(models.Model):
     TYPE_CHOICES = (
-        'Home', 'home',
-        'Apartment', 'apartment',
+        ('Home', 'home',),
+        ('Apartment', 'apartment'),
     )
 
     title = models.CharField(max_length=100)
@@ -14,11 +14,11 @@ class Listing(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=3)
     rooms = models.IntegerField()
     type = models.CharField(max_length=100, choices=TYPE_CHOICES)
+    # owner = models.ForeignKey('User', on_delete=models.CASCADE)
+    # review = models.ForeignKey('Review', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    # Date of create
-    # Date of update
-    # Actual
-    # Owner
-    # Reviews
 
 # Create your models here.
