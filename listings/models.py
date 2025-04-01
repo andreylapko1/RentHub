72effa1f091355bab90aa91104d9da6d148e7fa0
AndreyLapko1 <andreycgo765@gmail.com>
2025-04-01 11:04:37 +0300

    add Listing model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
+
+class Listing(models.Model):
+    TYPE_CHOICES = (
+        'Home', 'home',
+        'Apartment', 'apartment',
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    location = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    rooms = models.IntegerField()
+    type = models.CharField(max_length=100, choices=TYPE_CHOICES)
+
+    # Date of create
+    # Date of update
+    # Actual
+    # Owner
+    # Reviews
+
 # Create your models here.
