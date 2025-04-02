b791f0edd6f6a9894962c1add24ffe6ed7c4dc4f
AndreyLapko1 <andreycgo765@gmail.com>
2025-04-02 10:14:50 +0300

    add registerView

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     is_owner = models.BooleanField(default=False)
 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+
+        if not (username and password and email):
+            return Response({'message': 'Fields are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({'message': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+        User.objects.create_user(username=username, email=email, password=password)
+        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+
+
 
 # Create your views here.
