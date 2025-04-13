8bad52eb8253da36b4e01230e42c1e03a0b7e4fd
AndreyLapko1 <andreycgo765@gmail.com>
2025-04-13 19:29:45 +0300

    add create listing template and view

--- listings/form.py
+++ listings/form.py
@@ -5,11 +5,11 @@ from listings.models import Listing
 
 
 class ListingCreateForm(forms.ModelForm):
+    location = forms.ModelChoiceField(queryset=City.objects.filter(country_id=57),
+                                      empty_label="Select a city",
+                                      required=True
+                                      )
     class Meta:
         model = Listing
         fields = ['title', 'description', 'location' ,'price', 'rooms' ,'type' ,'image']
 
-        location = forms.ModelChoiceField(queryset=City.objects.filter(country_id=57),
-                                          empty_label="Select a city",
-                                          required=True
-                                          )
--- listings/urls.py
+++ listings/urls.py
@@ -14,7 +14,7 @@ from .views import ListingListView
 urlpatterns = [
     path('', ListingListView.as_view({"get": "list"}), name='listings_list'),
     path('<int:pk>/', ListingRetrieveView.as_view(), name='listing_detail'),
-    path('create/', ListingCreateView.as_view(), name='listings_create'),
+    path('create/', ListingCreateView.as_view(), name='listing_create'),
     path('my/', UserListingListView.as_view(), name='listings_user_list'),
     path('userlist/', UserList.as_view(), name='userlist'),
     path('my/<int:pk>/', ListingRetrieveUpdateView.as_view(), name='my_lisings'),
